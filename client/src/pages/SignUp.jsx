056d924fdb9b483ad77dcf5719105a45a0d06f37
tryhafsops <hafsahill17@gmail.com>
2024-04-05 15:05:08 +0500

    added sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,56 +1,117 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setErrorMessage('Unavailable Username/Email');
+        setLoading(false);
+        return;
+        
+        
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage('There has been a problem. Try Again');
+      setLoading(false);
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
         <div className='flex-1'>
-        <Link to="/" className=' font-bold dark:text-white'>
-            <span className='px-2 py-1 bg-sky-300 rounded-lg text-white text-7xl'>Hafsa</span>
+        <Link to="/" className='  font-bold dark:text-white'>
+            <span className='px-2 py-1 bg-green-300 rounded-lg text-white text-7xl'>Hafsa</span>
             <span className='px-1 py-1 text-5xl'>Blog</span>
         </Link>
-        <p className='text-sm mt-5'>
-          This is Hafsa Blog
-        </p>
+          <p className='text-sm mt-5'>
+            This is Hafsa Blog
+          </p>
         </div>
+        {/* right */}
+
         <div className='flex-1'>
-          <form className='flex flex-col gap-5'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Username'/>
+              <Label value='Username' />
               <TextInput
-              type='text'
-              placeholder='username'
-              id='username'/>
+                type='text'
+                placeholder='username'
+                id='username'
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <Label value='Email'/>
+              <Label value='Email' />
               <TextInput
-              type='text'
-              placeholder='email'
-              id='Email'/>
+                type='email'
+                placeholder='email'
+                id='email'
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <Label value='Password'/>
+              <Label value='Password' />
               <TextInput
-              type='text'
-              placeholder='password'
-              id='password'/>
-              
+                type='password'
+                placeholder='password'
+                id='password'
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToBlue' type='submit' >
-                Sign Up
+            <Button
+              gradientDuoTone='tealToLime'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
-          <div className='flex gap-2 text-sm mt-4'>
+          <div className='flex gap-2 text-sm mt-5'>
             <span>Already Have An Account?</span>
-            <Link to='/sign-in' className='text-orange-500'>Sign In</Link>
+            <Link to='/sign-in' className='text-orange-500'>
+              Sign In
+            </Link>
           </div>
-         
-
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
