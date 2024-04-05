01c17ee8f1bcbc8daa849ea5315fc9fdb7b3c298
tryhafsops <hafsahill17@gmail.com>
2024-04-06 00:05:07 +0500

    completed sign in page ui and functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,109 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ( !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setErrorMessage('Invalid Information');
+        setLoading(false);
+        return;
+        
+        
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage('There has been a problem. Try Again');
+      setLoading(false);
+    }
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+        <Link to="/" className='  font-bold dark:text-white'>
+            <span className='px-2 py-1 bg-green-300 rounded-lg text-white text-7xl'>Hafsa</span>
+            <span className='px-1 py-1 text-5xl'>Blog</span>
+        </Link>
+          <p className='text-sm mt-5'>
+            This is Hafsa Blog
+          </p>
+        </div>
+        {/* right */}
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Email' />
+              <TextInput
+                type='email'
+                placeholder='email'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Password' />
+              <TextInput
+                type='password'
+                placeholder='*********'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone='greenToBlue'
+              type='submit'
+              disabled={loading}
+              size='lg'
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't Have an Account?</span>
+            <Link to='/sign-up' className='text-orange-500'>
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
