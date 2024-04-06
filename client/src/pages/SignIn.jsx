64d4348c56a1ffced0940e86a231ef41ad26084c
tryhafsops <hafsahill17@gmail.com>
2024-04-06 11:01:56 +0500

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,23 +1,28 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if ( !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -25,19 +30,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setErrorMessage('Invalid Information');
-        setLoading(false);
-        return;
-        
-        
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage('There has been a problem. Try Again');
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -58,19 +59,19 @@ export default function SignIn() {
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Email' />
+              <Label value='Your email' />
               <TextInput
                 type='email'
-                placeholder='email'
+                placeholder='name@provider.com'
                 id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Password' />
+              <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='*********'
+                placeholder='******'
                 id='password'
                 onChange={handleChange}
               />
@@ -92,6 +93,7 @@ export default function SignIn() {
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
+            
             <span>Don't Have an Account?</span>
             <Link to='/sign-up' className='text-orange-500'>
               Sign Up
@@ -107,3 +109,10 @@ export default function SignIn() {
     </div>
   );
 }
+
+
+
+
+
+
+
