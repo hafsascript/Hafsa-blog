a7f1836f90fc839e2b3ec05ebab2fff327661db3
tryhafsops <hafsahill17@gmail.com>
2024-04-20 18:16:02 +0500

    added side panel to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashboardPanel from '../components/DashboardPanel';
+import DashboardProfile from '../components/DashboardProfile';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab,setTab] = useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-70'>
+        {/*SidePanel*/}
+        <DashboardPanel/>
+      </div>
+      
+      {/* Profile, dashboard features */}
+      {tab === 'profile' && <DashboardProfile/>} 
+
+    </div>
   )
 }
